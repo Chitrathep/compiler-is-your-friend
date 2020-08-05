fcac75f53265386faf31fb0a5c2968b6130e5b07
Ic3 <chitrathep.c@codium.co>
2020-08-05 13:44:08 +0700

    exercise 2

--- 2.immutability.ts
+++ 2.immutability.ts
@@ -5,10 +5,12 @@ The function below is a "safe divide" function. That is, it promises not to thro
 
 Questions:
 1) Spot and describe the cause of the bug that causes the "divide by zero" to occur.
+    - มันขึ้น Infinity ครับไม่เกิด error
 2) Now, make a *single* code change on line 14:
 2.1) without changing the number 42;
 2.2) such that cause of the bug can be spotted by the compiler at *compile time*.
 3) What did you learn from this exercise?
+    - พึ่งรู้ว่ามี Infinity เกิดขึ้นตอนหารด้วย 0 ครับ
 */
 function safeDivide(): number {
     let x: number = 42;
@@ -19,3 +21,4 @@ function safeDivide(): number {
 
     return 42 / x;
 }
+console.log(safeDivide());
